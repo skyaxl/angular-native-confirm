36e4203b4df8f7b2ce1498edfd648c4c2923d0ff
Michael J. Calkins <michaeljamescalkins@gmail.com>
2014-12-05 07:05:41 -0800

    Create confirm.js

---
+++ confirm.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('angular-native-confirm')
+  .service('Confirm', function ($window) {
+    return {
+      action: function(message, successCb, canceledCb) {
+        var hasActionBeenConfirmed = $window.confirm(message);
+
+        if (hasActionBeenConfirmed) {
+          return _.isFunction(successCb) && successCb();
+        }
+
+        return _.isFunction(canceledCb) && canceledCb();
+      }
+    };
+  });
