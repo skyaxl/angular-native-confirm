e2b055ba27fc7f6a1b4fa9082125da426285accb
Michael J. Calkins <michaeljamescalkins@gmail.com>
2014-12-05 07:05:02 -0800

    Create confirm.spec.js

---
+++ tests/confirm.spec.js
@@ -0,0 +1,18 @@
+'use strict';
+// THIS DOESN'T DO ANYTHING YET
+describe('Service: confirm', function () {
+
+  // load the service's module
+  beforeEach(module('csk3App'));
+
+  // instantiate service
+  var Confirm;
+  beforeEach(inject(function (_Confirm_) {
+    Confirm = _Confirm_;
+  }));
+
+  it('should do something', function () {
+    expect(!!Confirm).toBe(true);
+  });
+
+});
